fce7eb84a85846c42ca4c8965977bab19de5399a
d13g0hornburg <diegohornburg@gmail.com>
2024-09-09 08:05:33 -0300

    My first commit 09.09

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Define a classe MostrarOcultar que estende a classe Component do React
+class App extends Component {
+  // O construtor inicializa o estado e faz o bind do método alternarVisibilidade
+  constructor(props) {
+    super(props); // Chama o construtor da classe pai (Component)
+    this.state = {
+      visivel: false // Define o estado inicial, com a propriedade 'visivel' como false (texto oculto)
+    };
+    // Faz o bind do método alternarVisibilidade para que ele possa acessar o 'this' do componente
+    this.alternarVisibilidade = this.alternarVisibilidade.bind(this);
+  }
+
+  // Método que alterna a visibilidade do texto
+  alternarVisibilidade() {
+    // Atualiza o estado: inverte o valor de 'visivel'
+    this.setState(prevState => ({
+      visivel: !prevState.visivel // Se 'visivel' for true, muda para false e vice-versa
+    }));
+  }
+
+  // Método render que define o que será exibido na tela
+  render() {
+    return (
+      <div>
+        {/* Botão que chama o método alternarVisibilidade ao ser clicado */}
+        <button onClick={this.alternarVisibilidade}>
+          {/* O texto do botão muda conforme o estado: 'Mostrar Texto' ou 'Esconder Texto' */}
+          {this.state.visivel ? 'Esconder' : 'Mostrar'} Texto
+        </button>
+        {/* O texto só é exibido se o estado 'visivel' for true */}
+        {this.state.visivel && <p>Este é o texto que pode ser mostrado ou escondido.</p>}
+      </div>
+    );
+  }
 }
 
+// Exporta o componente para que possa ser importado e usado em outros arquivos
 export default App;
